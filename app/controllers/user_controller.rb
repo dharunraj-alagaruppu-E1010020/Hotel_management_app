7217d8f4fcd538283a7b12db9a6a4b6c74fee755
dharunraj-alagaruppu <153732820+dharunraj-alagaruppu@users.noreply.github.com>
2024-01-09 13:37:23 +0530

    final dev to main push

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -10,9 +10,62 @@ class UserController < ApplicationController
     end
 
     def create_user
-        @user = User.new(user_params)
+        user = User.new # Create new object for User model class
+        user.name = params[:name]
+        user.phone_number = params[:phone_number]
+        user.password = params[:password]
+        type_role = params[:role] # perameter value saved into the local variable type_role
+
+        if Role.is_present_role?(type_role) # if that class method return true that remaining processes will continue
+            retrive_role_id =  Role.find_by(role: type_role)&.id
+            user.role_id = retrive_role_id
+        else
+            raise ActiveRecord::RecordNotFound, " Undefined role : #{type_role} "
+        end
+
+        if user.save 
+            render json: { message: 'Entry created successfully' }, status: :created
+         else
+            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity # 422
+        end
         
     end
 
-     
+    def login
+        user = User.find_by(phone_number: params[:phone_number])
+
+      if user && User.find_by(password: params[:password])
+
+        if user.role_id == Role.find_by(role: 'admin')&.id
+            render json: { message: 'Admin login successful' }, status: :created
+          else
+            render json: { message: 'User login successful' }, status: :created
+          end
+      else
+        render json: { message: ' Invalid phone number or password' }, status: :created
+        
+      end
+
+    end
+
+    private
+
+    def validate_name
+        rescue RuntimeError => e
+         flash[:error] = e.message
+         redirect_to root_path
+    end
+
+    def validate_phone_number
+       rescue RuntimeError => e
+        flash[:error] = e.message
+        redirect_to root_path  
+    end
+
+    def validate_password
+       rescue RuntimeError => e
+        flash[:error] = e.message
+        redirect_to root_path
+    end
+
 end
--- db/migrate/20240105053021_create_roles.rb
+++
@@ -1,8 +0,0 @@
-class CreateRoles < ActiveRecord::Migration[7.1]
-  def change
-    create_table :roles do |t|
-      t.string :role
-      t.timestamps
-    end
-  end
-end
